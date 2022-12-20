9fc4134e85fd7f589626bcab86148e14e2fdd780
jeno <Jenodenslagen@hotmail.com>
2022-12-20 14:15:33 +0100

    deletes/changes
    LMAO

--- snowworld/Assets/Scripts/PlayerMovements.cs
+++ snowworld/Assets/Scripts/PlayerMovements.cs
@@ -5,28 +5,48 @@ using UnityEngine;
 public class PlayerMovements : MonoBehaviour
 {
     private Rigidbody2D rb;
-    [SerializeField] float speed = 6;
-    public Transform groundcheck;
-    public float GroundCheckRadius;
-    public LayerMask groundlayer;
-    private bool isTouchingGround;
+    [SerializeField] float movespeed = 6;
+    private SpriteRenderer sprite;
+
+    private Animator anim;
+    private float dirX;
+    [SerializeField]private float jumpforce = 14f;
     // Start is called before the first frame update
    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        isTouchingGround = Physics2D.OverlapCircle(groundcheck.position, GroundCheckRadius, groundlayer);
 
-        float dirX = Input.GetAxis("Horizontal");
-            rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
-        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        dirX = Input.GetAxisRaw("Horizontal");
+            rb.velocity = new Vector2(dirX * movespeed, rb.velocity.y);
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x,13);
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
         }
-        
+        updateanimationupdate();
+    }
+    private void updateanimationupdate()
+    { 
+        if (dirX > 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("running", false);
+            sprite.flipX = false;
+        }
     }
 }
